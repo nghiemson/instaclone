931896566fc13af5932ce89241c70d3e012f071b
sonnl <sonnghiemaz@gmail.com>
2023-01-31 16:33:47 +0700

    social login

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:instaclone/state/auth/backend/authenticator.dart';
 import 'firebase_options.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,54 +26,39 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blueGrey,
         indicatorColor: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Home Page'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          TextButton(
+            onPressed: () async {
+              final res = await Authenticator().loginWithGoogle();
+              print(res);
+            },
+            child: const Text('Sign In with Google'),
+          ),
+          TextButton(
+            onPressed: () async {
+              final res = await Authenticator().loginWithFB();
+              print(res);
+            },
+            child: const Text('Sign In with Facebook'),
+          )
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/state/auth/models/auth_state.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/foundation.dart' show immutable;
+import 'package:instaclone/state/auth/models/auth_result.dart';
+import 'package:instaclone/state/posts/typedefs/user_id.dart';
+
+@immutable
+class AuthState {
+  final AuthResult? result;
+  final bool isLoading;
+  final UserId? userId;
+
+  const AuthState({
+    required this.result,
+    required this.userId,
+    required this.isLoading,
+  });
+
+  const AuthState.unknown()
+      : result = null,
+        isLoading = false,
+        userId = null;
+
+  AuthState copiedWithIsLoading(bool isLoading) => AuthState(
+        result: result,
+        userId: userId,
+        isLoading: isLoading,
+      );
+
+  @override
+  bool operator ==(covariant AuthState other) =>
+      identical(this, other) ||
+      (result == other.result &&
+          isLoading == other.isLoading &&
+          userId == other.userId);
+
+  @override
+  int get hashCode => Object.hash(
+        result,
+        isLoading,
+        userId,
+      );
+}
