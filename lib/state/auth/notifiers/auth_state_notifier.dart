a0288a93c89528fc0c86d11ac3bd640a027eff00
sonnl <sonnl@savisgroup.vn>
2023-06-08 18:30:29 +0700

    upgrade lib

--- lib/state/auth/notifiers/auth_state_notifier.dart
+++ lib/state/auth/notifiers/auth_state_notifier.dart
@@ -2,9 +2,13 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:instaclone/state/auth/backend/authenticator.dart';
 import 'package:instaclone/state/auth/models/auth_result.dart';
 import 'package:instaclone/state/auth/models/auth_state.dart';
+import 'package:instaclone/state/user_info/backend/user_info_storage.dart';
+
+import '../../posts/typedefs/user_id.dart';
 
 class AuthStateNotifier extends StateNotifier<AuthState> {
   final _authenticator = const Authenticator();
+  final _userInfoStorage = const UserInfoStorage();
   AuthStateNotifier() : super(const AuthState.unknown()) {
     if (_authenticator.isAlreadyLoggedIn) {
       state = AuthState(
@@ -23,8 +27,28 @@ class AuthStateNotifier extends StateNotifier<AuthState> {
 
   Future<void> loginWithGg() async {
     state = state.copiedWithIsLoading(true);
-    final res = await _authenticator.loginWithGoogle();
+    final result = await _authenticator.loginWithGoogle();
     final userId = _authenticator.userId;
-    if (res == AuthResult.success && userId != null) {}
+    if (result == AuthResult.success && userId != null) {
+      await saveUserInfo(userId: userId);
+    }
+    state = AuthState(result: result, userId: userId, isLoading: false);
   }
+
+  Future<void> loginWithFb() async {
+    state = state.copiedWithIsLoading(true);
+    final result = await _authenticator.loginWithFB();
+    final userId = _authenticator.userId;
+    if (result == AuthResult.success && userId != null) {
+      await saveUserInfo(userId: userId);
+    }
+    state = AuthState(result: result, userId: userId, isLoading: false);
+  }
+
+  Future<void> saveUserInfo({required UserId userId}) =>
+      _userInfoStorage.saveUserInfo(
+        userId: userId,
+        displayName: _authenticator.displayName,
+        email: _authenticator.email,
+      );
 }
